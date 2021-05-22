8cac86a57fffe8c4d441a3b74182290c60a0df50
Arthur Street <arthurstreet@yahoo.com>
2021-05-22 20:58:19 +1000

    implement some code that could do the training if it works

--- rl_games/dqn/nac_player.py
+++ rl_games/dqn/nac_player.py
@@ -19,7 +19,7 @@ from collections import defaultdict
 
 from rl_games.core.game import State, Action, Game
 from rl_games.neural.neural_network import NeuralNetwork
-from rl_games.games.nac import NacState, x_marker, o_marker, empty_square
+from rl_games.games.nac import Nac, NacState, NacAction, x_marker, o_marker, empty_square
 
 
 m = {empty_square: 0, x_marker: 1, o_marker: 2}
@@ -52,30 +52,50 @@ def get_nac_state_index(state: NacState) -> int:
     board_index = sum(v * 3 ** i for i, v in enumerate(one_d))
     return board_index + 0 if state.next_turn == x_marker else 3 ** len(one_d)
 
-def get_onehot_input_from_index(index: int, size: int) -> np.ndarray:
+
+def get_onehot_vector_from_index(index: int, size: int) -> np.ndarray:
     """
-    >>> get_onehot_input_from_index(0, 5)
-    array([1, 0, 0, 0, 0])
-    >>> get_onehot_input_from_index(3, 4)
-    array([0, 0, 0, 1])
+    >>> get_onehot_vector_from_index(0, 5)
+    array([[1, 0, 0, 0, 0]])
+    >>> get_onehot_vector_from_index(3, 4)
+    array([[0, 0, 0, 1]])
     """
     x = np.zeros(size, dtype=int)
     x[index] = 1
     return x.reshape(1, size) # np.array([1 if i == index else 0 for i in range(size)])
 
+
 def get_onehot_nac_input(state: NacState) -> np.ndarray:
     """
     >>> board = [[empty_square,] * 2,] * 2
     >>> s = get_onehot_nac_input(NacState(board, x_marker))
-    >>> len(s), s[:5]
-    (162, array([1, 0, 0, 0, 0]))
+    >>> s.shape, s[:, :5]
+    ((1, 162), array([[1, 0, 0, 0, 0]]))
     >>> board[0][1] = x_marker
     >>> s = get_onehot_nac_input(NacState(board, o_marker))
-    >>> s[:5], s[81:86]
-    (array([0, 0, 0, 0, 0]), array([1, 0, 0, 0, 0]))
+    >>> s[:, :5], s[:, 81:86]
+    (array([[0, 0, 0, 0, 0]]), array([[1, 0, 0, 0, 0]]))
     """
     size = 2 * 3 ** len(state.board) ** 2
-    return get_onehot_input_from_index(get_nac_state_index(state), size)
+    return get_onehot_vector_from_index(get_nac_state_index(state), size)
+
+
+def get_nac_action_and_value_from_onehot_output(output: np.ndarray, legal_actions: Sequence[NacAction]) -> Tuple[NacAction, float]:
+    size = np.sqrt(len(output))  # Or from game.size
+    mask_to_clear = np.ones((size, size), dtype=bool)
+    for action in legal_actions:
+        mask_to_clear[action.row][action.col] = False
+    # TODO: output the legal actions as a mask in the first place
+    masked_output = output.copy()
+    masked_output[mask_to_clear] = -1
+    max_index = np.argmax(masked_output)
+    max_value = masked_output.reshape(len(output))[max_index]
+    return NacAction(row=max_index // size, col=max_index % size), max_value
+
+
+def get_onehot_index_from_nac_action(game: Nac, action: NacAction) -> int:
+    return action.row * game.size + action.col
+
 
 @dataclass
 class Player(Generic[State, Action]):
@@ -89,6 +109,8 @@ class Player(Generic[State, Action]):
     hidden_size: int = 18
     num_actions: int = 9
     get_input_vector: Callable[[NacState], np.ndarray] = get_onehot_nac_input  # TODO: [State]
+    get_action_and_value_from_output: Callable[[np.ndarray, Sequence[NacAction]], Tuple[NacAction, float]] = get_nac_action_and_value_from_onehot_output  # TODO: [Action]
+    get_onehot_index_from_action: Callable[[Nac, NacAction], int] = get_onehot_index_from_nac_action  # TODO: [Action, Game]
 
     def __post_init__(self, *args: Any, **kwargs: Any) -> None:
         self.model = NeuralNetwork(
@@ -115,8 +137,8 @@ class Player(Generic[State, Action]):
         random.shuffle(actions)
         if len(actions) == 0:
             raise IndexError(f'No actions available from {state}')
-        best_action_index = np.argmax(self.model.predict(self.get_input_vector(state)))
-        return actions[best_action_index]
+        model_output = self.model.predict(self.get_input_vector(state))
+        return self.get_action_and_value_from_output(model_output, actions)[0]
 
     def value(self, game: Game, state: State) -> float:
         """
@@ -129,7 +151,8 @@ class Player(Generic[State, Action]):
         """
         actions = list(game.get_actions(state))
         if len(actions):
-            return 1 # max(self.action_value.get((state, action), 0) for action in actions)
+            model_output = self.model.predict(self.get_input_vector(state))
+            return self.get_action_and_value_from_output(model_output, actions)[1]
         # If no actions are possible, the game must be over, and the value is 0.
         return 0
 
@@ -153,7 +176,8 @@ class Player(Generic[State, Action]):
         >>> player.update_action_value(game, 2, True, 4, 0)
         >>> player.update_action_value(game, 0, True, 2, 0)
         """
-        pass
-        # self.action_value[old_state, action] += reward + self.learning_rate * (
-        #     self.discount_factor * self.value(game, new_state)
-        #     - self.action_value[old_state, action])
+        target = reward + self.discount_factor * np.max(
+            self.model.predict(self.get_input_vector(new_state)))
+        target_vector = self.model.predict(self.get_input_vector(old_state))
+        target_vector[self.get_onehot_index_from_action(action)] = target
+        self.model.train([self.get_input_vector(old_state)], [target_vector], num_iterations=1)
