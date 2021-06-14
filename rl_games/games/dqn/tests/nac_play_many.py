3c2735dec7330f0da0030552cf4bd4da1ccd484d
Arthur Street <arthurstreet@yahoo.com>
2021-06-14 18:59:20 +1000

    move tests

---
+++ rl_games/games/dqn/tests/nac_play_many.py
@@ -0,0 +1,26 @@
+from typing import Sequence, Dict, Any
+
+from rl_games.q_learner.player import Player
+from rl_games.core.play import play_many
+from rl_games.games.nac import Nac
+
+
+def nac_play_many(
+    players: Sequence[Player],
+    *args: Any,
+    **kwargs: Any
+) -> Dict[str, float]:
+    """
+    >>> import random
+    >>> from rl_games.games.nac import NacState, NacAction
+    >>> from rl_games.dqn.dqn_player import DqnPlayer
+    >>> from ..nac import NacDqnSetup
+    >>> random.seed(2)
+    >>> players = [DqnPlayer[NacState, NacAction]('X', NacDqnSetup()), DqnPlayer[NacState, NacAction]('O', NacDqnSetup())]
+    >>> nac_play_many(players, 20)
+    {'O': 0.1, 'X': 0.9}
+
+    TODO: improve this test.
+    """
+    game = Nac()
+    return play_many(game, players, *args, **kwargs)
