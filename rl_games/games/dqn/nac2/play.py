0256de1bfaf408d7a7e79e6d00e15d18868c91b2
Arthur Street <arthurstreet@yahoo.com>
2021-06-20 20:16:00 +1000

    first pass at non-one-hot nac state encoding, fails terribly

---
+++ rl_games/games/dqn/nac2/play.py
@@ -0,0 +1,33 @@
+# This experimental version of DQN NAC uses a non-one-hot state encoding.
+# It fails terribly.
+#
+# Run with:
+#     python -m rl_games.games.dqn.nac2.play
+
+from typing import Sequence, Tuple
+
+from rl_games.dqn.dqn_player import DqnPlayer
+from rl_games.core.play import play_many
+from rl_games.core.play_human import play_human_ui
+from rl_games.core.player import Player
+from rl_games.core.game import Game
+from rl_games.games.nac import Nac, NacState, NacAction
+from rl_games.games.tqdm import range_with_timer
+from .setup import NacDqnSetup
+
+
+def get_sample_game_and_trained_players(num_rounds: int = 10000, initial_explore_chance: float = 0.25) -> Tuple[Game, Sequence[Player]]:
+    game = Nac()
+
+    players = [
+        DqnPlayer[NacState, NacAction](game.markers[0], NacDqnSetup(), explore_chance=initial_explore_chance),
+        DqnPlayer[NacState, NacAction](game.markers[1], NacDqnSetup(), explore_chance=initial_explore_chance),
+    ]
+
+    play_many(game, players, range_with_timer(num_rounds), reduce_explore_chance=True)
+    return game, players
+
+
+if __name__ == '__main__':
+    game1, players1 = get_sample_game_and_trained_players()
+    play_human_ui(game1, players1)
---
+++ rl_games/games/dqn/nac2/setup.py
@@ -0,0 +1,44 @@
+from dataclasses import dataclass
+import numpy as np
+
+from rl_games.games.nac import Nac, NacState, NacAction, empty_square
+from rl_games.dqn.setup import (
+    DqnSetup,
+    GameAndStateToVector,
+    GameAndStateToActionMask,
+    OutputToActionAndValue,
+    ActionToIndex,
+    StateVector,
+)
+from ..nac.setup import get_nac_action_and_value_from_onehot_output, get_nac_action_mask, get_onehot_index_from_nac_action
+
+# This experimental version of DQN NAC uses a non-one-hot state encoding,
+# with just one state per position, with values -1 (X), 0 (empty) or 1 (O).
+
+
+def get_nac_input(game: Nac, state: NacState) -> StateVector:
+    """
+    >>> game = Nac(size=2)
+    >>> board = [[empty_square for _ in range(2)] for _ in range(2)]
+    >>> get_nac_input(game, NacState(board, next_player_index=0))
+    array([[0, 0, 0, 0]])
+    >>> board[0][1] = game.markers[0]
+    >>> get_nac_input(game, NacState(board, next_player_index=1))
+    array([[ 0, -1,  0,  0]])
+    >>> board[1][1] = game.markers[1]
+    >>> get_nac_input(game, NacState(board, next_player_index=0))
+    array([[ 0, -1,  0,  1]])
+    """
+    m = {empty_square: 0, game.markers[0]: -1, game.markers[1]: 1}
+    return StateVector(np.array([m[e] for row in state.board for e in row], dtype=int).reshape(1, -1))
+
+
+@dataclass
+class NacDqnSetup(DqnSetup[NacState, NacAction]):
+    num_states: int = 9
+    hidden_size: int = 18
+    num_actions: int = 9
+    get_input_vector: GameAndStateToVector = get_nac_input
+    get_action_mask: GameAndStateToActionMask = get_nac_action_mask
+    get_action_and_value_from_output: OutputToActionAndValue = get_nac_action_and_value_from_onehot_output
+    get_onehot_index_from_action: ActionToIndex = get_onehot_index_from_nac_action
